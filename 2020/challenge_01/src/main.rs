1352bb9328891f303b5ea8bea8ac02cef6d9777d
bchhun <bchhun@cortexmedia.ca>
2020-12-01 08:38:45 -0500

    feat: challenge 1 in rust !

---
+++ 2020/challenge_01/src/main.rs
@@ -0,0 +1,38 @@
+use std::{
+    fs::File,
+    io::{prelude::*, BufReader},
+    path::Path,
+};
+
+fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect()
+}
+
+fn main() {
+    let lines = lines_from_file("src/input.txt");
+    let mut expenses = Vec::new();
+    expenses = lines
+        .iter()
+        .map(
+            |l| l.parse::<i32>().unwrap()
+        ).collect();
+    let year: i32 = 2020;
+    let mut target_expense: i32 = 0;
+    let mut target_diff: i32 = 0;
+
+    for expense in expenses.iter() {
+        let diff = year - expense;
+
+        if expenses.contains(&diff) {
+            target_expense = *expense;
+            target_diff = diff;
+            break;
+        }
+    }
+
+    println!("expense lines: {} x {} = {}", target_expense, target_diff, target_expense * target_diff);
+}
